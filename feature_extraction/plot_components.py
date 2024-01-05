0aaa5fafeb152165432e8589ddca4f7a77655ff9
Pierre-Louis Barbarant <plbarbarant@gmail.com>
2024-01-05 09:30:30 +0100

    Refactor feature extraction and add metadata visualization

--- feature_extraction/plot_components.py
+++ feature_extraction/plot_components.py
@@ -4,19 +4,22 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import pickle as pkl
+from sklearn.preprocessing import LabelEncoder
+
 
 # Paths to pca and ica features
 ica_aligned_path = Path(
-    "/data/parietal/store3/work/pbarbara/fmri2image_alignment/data/NSD/ica_sub-01/transformed_features.npy"
+    "/data/parietal/store3/work/pbarbara/fmri2image_alignment/feature_extraction/features/ica_aligned/extracted_features.npy"
 )
 ica_unaligned_path = Path(
-    "/data/parietal/store3/work/pbarbara/fmri2image_alignment/data/NSD/ica_sub-01/transformed_features.npy"
+    "/data/parietal/store3/work/pbarbara/fmri2image_alignment/feature_extraction/features/ica_unaligned/extracted_features.npy"
 )
 pca_aligned_path = Path(
-    "/data/parietal/store3/work/pbarbara/fmri2image_alignment/data/NSD/pca_sub-01/transformed_features.npy"
+    "/data/parietal/store3/work/pbarbara/fmri2image_alignment/feature_extraction/features/pca_aligned/extracted_features.npy"
 )
 pca_unaligned_path = Path(
-    "/data/parietal/store3/work/pbarbara/fmri2image_alignment/data/NSD/pca_sub-01/transformed_features.npy"
+    "/data/parietal/store3/work/pbarbara/fmri2image_alignment/feature_extraction/features/pca_unaligned/extracted_features.npy"
 )
 
 ica_aligned = np.load(ica_aligned_path)
@@ -25,17 +28,87 @@ pca_aligned = np.load(pca_aligned_path)
 pca_unaligned = np.load(pca_unaligned_path)
 
 
-labels = np.concatenate([np.ones(902) * i for i in range(1, 9)])
+subjects = np.concatenate([np.ones(902) * i for i in range(2, 9)])
+
 
 fig, axes = plt.subplots(1, 4, figsize=(12, 4))
-axes[0].scatter(ica_aligned[:, 0], ica_aligned[:, 1], c=labels)
+axes[0].scatter(
+    ica_aligned[:, 0],
+    ica_aligned[:, 1],
+    c=subjects,
+    cmap="tab10",
+    label="subject",
+)
 axes[0].set_title("ICA aligned")
-axes[1].scatter(ica_unaligned[:, 0], ica_unaligned[:, 1], c=labels)
+axes[1].scatter(
+    ica_unaligned[:, 0], ica_unaligned[:, 1], c=subjects, cmap="tab10"
+)
 axes[1].set_title("ICA unaligned")
-axes[2].scatter(pca_aligned[:, 0], pca_aligned[:, 1], c=labels)
+axes[2].scatter(pca_aligned[:, 0], pca_aligned[:, 1], c=subjects, cmap="tab10")
 axes[2].set_title("PCA aligned")
-axes[3].scatter(pca_unaligned[:, 0], pca_unaligned[:, 1], c=labels)
+axes[3].scatter(
+    pca_unaligned[:, 0], pca_unaligned[:, 1], c=subjects, cmap="tab10"
+)
 axes[3].set_title("PCA unaligned")
+for ax in axes:
+    ax.set_aspect("equal")
+    ax.set_box_aspect(1)
 plt.tight_layout()
-plt.legend()
+plt.show()
+
+
+# Path to coco annotations
+annotations_path = Path(
+    "/storage/store3/data/natural_scenes/info/coco_annotations/coco_categories.pkl"
+)
+
+metadata_path = Path(
+    "/storage/store3/work/pbarbara/fmri2image_alignment/data/NSD/nsd_stim_info_merged.csv"
+)
+
+# Load annotations
+with open(annotations_path, "rb") as f:
+    annotations = pkl.load(f)
+
+annotations = pd.DataFrame(annotations)
+annotations = annotations[["id", "supercat"]]
+annotations["id"] = annotations["id"].astype(int) - 1
+
+# Load metadata
+metadata = pd.read_csv(metadata_path)
+
+# Merge annotations and metadata on nsdId
+metadata = metadata.merge(annotations, left_on="nsdId", right_on="id")
+metadata = metadata[
+    (metadata["shared1000"] == True) & (metadata["subject1_rep0"] <= 22274)
+]
+
+metadata = metadata[["nsdId", "supercat"]]
+
+labels = np.concatenate([np.array(metadata["supercat"])] * 7).flatten()
+labels = LabelEncoder().fit_transform(labels)
+
+fig, axes = plt.subplots(1, 4, figsize=(12, 4))
+axes[0].scatter(
+    ica_aligned[:, 0],
+    ica_aligned[:, 1],
+    c=labels,
+    cmap="tab10",
+)
+axes[0].set_title("ICA aligned")
+axes[1].scatter(
+    ica_unaligned[:, 0], ica_unaligned[:, 1], c=labels, cmap="tab10"
+)
+axes[1].set_title("ICA unaligned")
+axes[2].scatter(pca_aligned[:, 0], pca_aligned[:, 1], c=labels, cmap="tab10")
+axes[2].set_title("PCA aligned")
+axes[3].scatter(
+    pca_unaligned[:, 0], pca_unaligned[:, 1], c=labels, cmap="tab10"
+)
+axes[3].set_title("PCA unaligned")
+# Set box aspect ratio
+for ax in axes:
+    ax.set_aspect("equal")
+    ax.set_box_aspect(1)
+plt.tight_layout()
 plt.show()
