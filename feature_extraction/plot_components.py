07ac58aecbd2900d19667c58486c8659855435c9
Pierre-Louis Barbarant <plbarbarant@gmail.com>
2024-01-05 12:33:30 +0100

    Update scatter plot axes limits and titles

--- feature_extraction/plot_components.py
+++ feature_extraction/plot_components.py
@@ -39,17 +39,25 @@ axes[0].scatter(
     cmap="tab10",
     label="subject",
 )
+axes[0].set_xlim(-20, 20)
+axes[0].set_ylim(-20, 20)
 axes[0].set_title("ICA aligned")
 axes[1].scatter(
     ica_unaligned[:, 0], ica_unaligned[:, 1], c=subjects, cmap="tab10"
 )
 axes[1].set_title("ICA unaligned")
+axes[1].set_xlim(-20, 20)
+axes[1].set_ylim(-20, 20)
 axes[2].scatter(pca_aligned[:, 0], pca_aligned[:, 1], c=subjects, cmap="tab10")
 axes[2].set_title("PCA aligned")
+axes[2].set_xlim(-1000, 1000)
+axes[2].set_ylim(-1000, 1000)
 axes[3].scatter(
     pca_unaligned[:, 0], pca_unaligned[:, 1], c=subjects, cmap="tab10"
 )
 axes[3].set_title("PCA unaligned")
+axes[3].set_xlim(-1000, 1000)
+axes[3].set_ylim(-1000, 1000)
 for ax in axes:
     ax.set_aspect("equal")
     ax.set_box_aspect(1)
@@ -95,17 +103,25 @@ axes[0].scatter(
     c=labels,
     cmap="tab10",
 )
+axes[0].set_xlim(-20, 20)
+axes[0].set_ylim(-20, 20)
 axes[0].set_title("ICA aligned")
 axes[1].scatter(
     ica_unaligned[:, 0], ica_unaligned[:, 1], c=labels, cmap="tab10"
 )
 axes[1].set_title("ICA unaligned")
+axes[1].set_xlim(-20, 20)
+axes[1].set_ylim(-20, 20)
 axes[2].scatter(pca_aligned[:, 0], pca_aligned[:, 1], c=labels, cmap="tab10")
 axes[2].set_title("PCA aligned")
+axes[2].set_xlim(-1000, 1000)
+axes[2].set_ylim(-1000, 1000)
 axes[3].scatter(
     pca_unaligned[:, 0], pca_unaligned[:, 1], c=labels, cmap="tab10"
 )
 axes[3].set_title("PCA unaligned")
+axes[3].set_xlim(-1000, 1000)
+axes[3].set_ylim(-1000, 1000)
 # Set box aspect ratio
 for ax in axes:
     ax.set_aspect("equal")
