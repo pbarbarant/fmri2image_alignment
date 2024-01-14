aa8b26022d1d07354e8bf9431cf13f0413efa88d
Pierre-Louis Barbarant <plbarbarant@gmail.com>
2024-01-14 11:29:48 +0100

    Add trainer script for DDPM model

---
+++ diffusion/trainer_ddpm.py
@@ -0,0 +1,43 @@
+# %%
+from ddpm import train_ddpm
+
+import wandb
+
+
+def launch():
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    args = parser.parse_args()
+    args.run_name = "DDPM_conditional"
+    args.epochs = 300
+    args.batch_size = 32
+    args.image_size = 64
+    args.mri_dim = 2
+    args.dataset_path = r"/data/parietal/store3/work/pbarbara/fmri2image_alignment/data/NSD/dataset_unaligned/dataset.csv"
+    args.device = "cuda"
+    args.lr = 3e-4
+    train_ddpm(args)
+
+
+if __name__ == "__main__":
+    # Initialize WandB
+    wandb.init(
+        project="fmri2image_alignment",
+        name="DDPM_conditional",
+        # mode="disabled",
+    )
+    launch()
+    wandb.finish()
+    # device = "cuda"
+    # model = UNet_conditional(mri_dim=2).to(device)
+    # model = nn.DataParallel(model)
+    # ckpt = torch.load(
+    #     "/data/parietal/store3/work/pbarbara/fmri2image_alignment/models/DDPM_conditional_aligned/ckpt.pt"
+    # )
+    # model.load_state_dict(ckpt)
+    # diffusion = Diffusion(img_size=64, device=device)
+    # n = 8
+    # y = torch.Tensor([6] * n).long().to(device)
+    # x = diffusion.sample(model, n, None)
+    # plot_images(x)
