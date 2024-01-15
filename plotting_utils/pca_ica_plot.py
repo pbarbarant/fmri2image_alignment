d126646e7296e2eff22a482a5166caec7bb822a5
Pierre-Louis Barbarant <plbarbarant@gmail.com>
2024-01-15 17:18:00 +0100

    Add and delete figures, update PCA and ICA plot

--- plotting_utils/pca_ica_plot.py
+++ plotting_utils/pca_ica_plot.py
@@ -1,11 +1,8 @@
 # %%
-import pickle as pkl
 from pathlib import Path
 
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
-from sklearn.preprocessing import LabelEncoder
 
 # Paths to ICA and PCA features
 ica_aligned_path = Path(
@@ -30,11 +27,12 @@ pca_unaligned = np.load(pca_unaligned_path)
 subjects = np.concatenate([np.ones(902) * i for i in range(2, 9)])
 
 
-fig, axes = plt.subplots(1, 4, figsize=(12, 3))
-fig.suptitle("First two components of ICA and PCA features")
+fig, axes = plt.subplots(1, 2, figsize=(15, 7))
+axes = axes.flatten()
+fig.suptitle("First two components of ICA features")
 axes[0].scatter(
-    ica_aligned[:, 0],
-    ica_aligned[:, 1],
+    ica_unaligned[:, 0],
+    ica_unaligned[:, 1],
     c=subjects,
     cmap="tab10",
     s=2,
@@ -43,146 +41,73 @@ axes[0].scatter(
 )
 axes[0].set_xlim(-20, 20)
 axes[0].set_ylim(-20, 20)
-axes[0].set_title("ICA aligned")
-axes[1].scatter(
-    ica_unaligned[:, 0],
-    ica_unaligned[:, 1],
-    c=subjects,
-    cmap="tab10",
-    s=2,
-    alpha=0.5,
-)
-axes[1].set_title("ICA unaligned")
-axes[1].set_xlim(-20, 20)
-axes[1].set_ylim(-20, 20)
-axes[2].scatter(
-    pca_aligned[:, 0],
-    pca_aligned[:, 1],
-    c=subjects,
-    cmap="tab10",
-    s=2,
-    alpha=0.5,
-)
-axes[2].set_title("PCA aligned")
-axes[2].set_xlim(-1000, 1000)
-axes[2].set_ylim(-1000, 1000)
-scatter = axes[3].scatter(
-    pca_unaligned[:, 0],
-    pca_unaligned[:, 1],
+axes[0].set_title("Unaligned data")
+scatter = axes[1].scatter(
+    ica_aligned[:, 0],
+    ica_aligned[:, 1],
     c=subjects,
     cmap="tab10",
     s=2,
     alpha=0.5,
 )
 handles, _ = scatter.legend_elements(prop="colors", alpha=0.8)
-axes[3].legend(
+axes[1].legend(
     handles,
-    [f"sub-0{i}" for i in range(1, 9)],
+    [f"sub-0{i}" for i in range(2, 9)],
     loc="center right",
-    bbox_to_anchor=(1.7, 0.5),
+    bbox_to_anchor=(1.25, 0.5),
 )
-axes[3].set_title("PCA unaligned")
-axes[3].set_xlim(-1000, 1000)
-axes[3].set_ylim(-1000, 1000)
-for ax in axes:
-    ax.set_aspect("equal")
-    ax.set_box_aspect(1)
-plt.tight_layout()
-plt.show()
+axes[1].set_title("Aligned data")
+axes[1].set_xlim(-20, 20)
+axes[1].set_ylim(-20, 20)
 
 # Save figure
 fig.savefig(
-    "/data/parietal/store3/work/pbarbara/fmri2image_alignment/figures/pca_ica_plot_subjects.png",
+    "/data/parietal/store3/work/pbarbara/fmri2image_alignment/figures/ica_plot.png",
     bbox_inches="tight",
 )
 
-# Path to coco annotations
-annotations_path = Path(
-    "/data/parietal/store3/data/natural_scenes/info/coco_annotations/coco_categories.pkl"
-)
 
-metadata_path = Path(
-    "/data/parietal/store3/work/pbarbara/fmri2image_alignment/data/NSD/nsd_stim_info_merged.csv"
-)
-
-# Load annotations
-with open(annotations_path, "rb") as f:
-    annotations = pkl.load(f)
-
-annotations = pd.DataFrame(annotations)
-annotations = annotations[["id", "supercat"]]
-annotations["id"] = annotations["id"].astype(int) - 1
-
-# Load metadata
-metadata = pd.read_csv(metadata_path)
-
-# Merge annotations and metadata on nsdId
-metadata = metadata.merge(annotations, left_on="nsdId", right_on="id")
-metadata = metadata[
-    (metadata["shared1000"] == True) & (metadata["subject1_rep0"] <= 22274)
-]
-
-metadata = metadata[["nsdId", "supercat"]]
-
-labels = np.concatenate([np.array(metadata["supercat"])] * 7).flatten()
-labels = LabelEncoder().fit_transform(labels)
-
-fig, axes = plt.subplots(1, 4, figsize=(12, 3))
-fig.suptitle("First two components of ICA and PCA features")
+fig, axes = plt.subplots(1, 2, figsize=(15, 7))
+axes = axes.flatten()
+fig.suptitle("First two components of PCA features")
 axes[0].scatter(
-    ica_aligned[:, 0],
-    ica_aligned[:, 1],
-    c=labels,
-    cmap="tab10",
-    s=2,
-    alpha=0.5,
-)
-axes[0].set_xlim(-20, 20)
-axes[0].set_ylim(-20, 20)
-axes[0].set_title("ICA aligned")
-axes[1].scatter(
-    ica_unaligned[:, 0],
-    ica_unaligned[:, 1],
-    c=labels,
-    cmap="tab10",
-    s=2,
-    alpha=0.5,
-)
-axes[1].set_title("ICA unaligned")
-axes[1].set_xlim(-20, 20)
-axes[1].set_ylim(-20, 20)
-axes[2].scatter(
-    pca_aligned[:, 0],
-    pca_aligned[:, 1],
-    c=labels,
-    cmap="tab10",
-    s=2,
-    alpha=0.5,
-)
-axes[2].set_title("PCA aligned")
-axes[2].set_xlim(-1000, 1000)
-axes[2].set_ylim(-1000, 1000)
-axes[3].scatter(
     pca_unaligned[:, 0],
     pca_unaligned[:, 1],
-    c=labels,
+    c=subjects,
     cmap="tab10",
     s=2,
     alpha=0.5,
 )
-axes[3].set_title("PCA unaligned")
-axes[3].set_xlim(-1000, 1000)
-axes[3].set_ylim(-1000, 1000)
-# Set box aspect ratio
+axes[0].set_title("Unaligned data")
+axes[0].set_xlim(-1000, 1000)
+axes[0].set_ylim(-1000, 1000)
+scatter = axes[1].scatter(
+    pca_aligned[:, 0],
+    pca_aligned[:, 1],
+    c=subjects,
+    cmap="tab10",
+    s=2,
+    alpha=0.5,
+)
+handles, _ = scatter.legend_elements(prop="colors", alpha=0.8)
+axes[1].legend(
+    handles,
+    [f"sub-0{i}" for i in range(2, 9)],
+    loc="center right",
+    bbox_to_anchor=(1.2, 0.5),
+)
+axes[1].set_title("Aligned data")
+axes[1].set_xlim(-1000, 1000)
+axes[1].set_ylim(-1000, 1000)
 for ax in axes:
     ax.set_aspect("equal")
     ax.set_box_aspect(1)
 plt.tight_layout()
 plt.show()
 
-
 # Save figure
 fig.savefig(
-    "/data/parietal/store3/work/pbarbara/fmri2image_alignment/figures/pca_ica_plot_labels.png",
+    "/data/parietal/store3/work/pbarbara/fmri2image_alignment/figures/pca_plot.png",
     bbox_inches="tight",
 )
