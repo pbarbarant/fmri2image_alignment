ad052a43856e130724b26649d4c90eb8a8ff7aaa
Pierre-Louis Barbarant <plbarbarant@gmail.com>
2024-01-16 14:13:33 +0100

    Remove FID metric from aligned_metric_eval.py

--- diffusion/aligned_metric_eval.py
+++ diffusion/aligned_metric_eval.py
@@ -6,7 +6,7 @@ import pandas as pd
 import torch
 import torch.nn as nn
 from ddpm import Diffusion
-from ignite.metrics import FID, SSIM
+from ignite.metrics import SSIM
 from modules import UNet_conditional
 from PIL import Image
 from scipy.linalg import sqrtm
